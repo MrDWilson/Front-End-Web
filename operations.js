c1929ea812a04ae1fcaac11c5c87ffc20b4beaa7
Danny Wilson <d.wilson2306@gmail.com>
2017-03-08 15:37:25 +0000

    Added logo animation (partially tested) and fixed some resizing issues.

--- operations.js
+++ operations.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    
+    layBricks();
+    
     $('#title').click(function () {
     
         hideOthers();
@@ -66,4 +69,41 @@ function hideOthers() {
     document.getElementById('contact-div').style.display = 'none';
     document.getElementById('about-div').style.display = 'none';
     document.getElementById('load-div').style.display = 'none';
+}
+
+function layBricks() {
+    var brick1 = document.getElementById('brick1');
+    var brick2 = document.getElementById('brick2');
+    var brick3 = document.getElementById('brick3');
+    var brick4 = document.getElementById('brick4');
+    var brick5 = document.getElementById('brick5');
+    var brick6 = document.getElementById('brick6');
+    
+    var pos = 0, id = setInterval(frame, 20);
+    
+    function frame(){
+        if(pos === 28) {
+            clearInterval(id);
+        } else{
+            pos++;
+            brick1.style.top = pos - 3 + 'px';
+            brick1.style.left = pos + 35 + 'px';
+            
+            brick2.style.top = pos - 3 + 'px';
+            brick2.style.left = pos + 15 + 'px';
+            
+            brick3.style.top = pos - 3 + 'px';
+            brick3.style.left = pos - 5 + 'px';
+            
+            brick4.style.top = pos - 13 + 'px';
+            brick4.style.left = pos + 25 + 'px';
+            
+            brick5.style.top = pos - 13 + 'px';
+            brick5.style.left = pos + 5 + 'px';
+            
+            brick6.style.top = pos - 23 + 'px';
+            brick6.style.left = pos + 15 + 'px';
+        }
+    }
+    
 }
